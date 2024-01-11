1dc15493a4208fc3004e1b1c15c791687eccc4c6
sahil-2810 <sahilg@argtechnologies.in>
2024-01-11 21:37:35 +0530

    Update example-dag.py

--- example-dag.py
+++ example-dag.py
@@ -8,23 +8,22 @@ from airflow.operators.dummy_operator import DummyOperator
 
 # Step 2: Initiating the default_args
 default_args = {
-        'owner' : 'airflow',
-        'start_date' : datetime(2022, 11, 12),
-
+    'owner': 'airflow',
+    'start_date': datetime(2022, 11, 12),
 }
 
 # Step 3: Creating DAG Object
 dag = DAG(dag_id='DAG-1',
-        default_args=default_args,
-        schedule_interval='@once', 
-        catchup=False
-    )
+          default_args=default_args,
+          schedule_interval='@once',
+          catchup=False
+          )
 
 # Step 4: Creating task
 # Creating first task
- start = DummyOperator(task_id = 'start', dag = dag)
+start = DummyOperator(task_id='start', dag=dag)
 # Creating second task 
- end = DummyOperator(task_id = 'end', dag = dag)
+end = DummyOperator(task_id='end', dag=dag)
 
- # Step 5: Setting up dependencies 
-start >> end 
+# Step 5: Setting up dependencies 
+start >> end
