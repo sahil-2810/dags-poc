fdc08cfb5ac8dd4fecbf795d19f10c0fb9c5b2d0
sahil-2810 <sahilg@argtechnologies.in>
2024-01-12 09:57:19 +0530

    Update hello_world_dag_copy.py

--- hello_world_dag_copy.py
+++ hello_world_dag_copy.py
@@ -14,8 +14,9 @@ default_args = {
 dag = DAG(
     'hello_world_dag_COPY',
     default_args=default_args,
-    description='A simple DAG with a Hello World task',
-    schedule_interval=timedelta(minutes=10),  # Set to run every 10 minutes
+    schedule_interval="*/2 * * * *",
+    description='A simple DAG with a Hello World task'
+    # schedule_interval=timedelta(minutes=10),  # Set to run every 10 minutes
 )
 
 # Define a Python function to run as a task
