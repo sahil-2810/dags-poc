38bb9b68bb819e8054bdc6a20778477123cbfe16
sahil-2810 <sahilg@argtechnologies.in>
2024-02-02 19:57:49 +0530

    reverted to original dag

--- be/dags/airflowDAG_AllocatorTest.py
+++ be/dags/airflowDAG_AllocatorTest.py
@@ -7,7 +7,6 @@ import pandas as pd
 import requests
 import redis
 import sys
-from redis.asyncio.sentinel import Sentinel
 from airflow import DAG
 from airflow.decorators import task
 from be.brompton.WorkerAllocator import *
@@ -58,22 +57,14 @@ with DAG(
         return df.values.tolist()
 
     @task
-    async def report_results(calc_results: List[List[Any]]):
+    def report_results(calc_results: List[List[Any]]):
         for calc in calc_results:
             print(f"Calculation {calc[0]}, with sort value {calc[1]}, consisting of expression {calc[4]} with inputs a={calc[2]} and b={calc[3]} has result {calc[5]}")
-        # sentinel = Sentinel([("redis", 26379)],sentinel_kwargs={'password': 'test@123'}, password='test@123')
-        # r = sentinel.master_for("mymaster")
-        # # print(await sentinel.discover_master("mymaster"))
-        # # print(sentinel)
-        # # print(r)
-        # ok = await r.set("key", "value123")
-        # val = await r.get("key")
-        # print(val)
+
     # Main flow
     allocated = allocate_workers()
     calc_results=execute_calculations.expand(allocated_calc_instances=allocated)
     report_results.expand(calc_results=calc_results)
-
 
 
 
