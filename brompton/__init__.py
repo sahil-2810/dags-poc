bd7fd532dbe09f7180922dde26a8164dbac1c3ce
sahil-2810 <sahilg@argtechnologies.in>
2024-02-02 09:58:43 +0530

    removed content

--- brompton/__init__.py
+++ brompton/__init__.py
@@ -1,7 +1,7 @@
 # from tasks import *
 # brompton/__init__.py
 
-from .WorkerAllocator import allocate, AllocateCandidate
-from .hello import print_hello
-
-__all__ = ["allocate", "AllocateCandidate"]
+# from .WorkerAllocator import allocate, AllocateCandidate
+# from .hello import print_hello
+#
+# __all__ = ["allocate", "AllocateCandidate"]
